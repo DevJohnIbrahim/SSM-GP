09c7838671c00ae46acac1d706f35e0ae827b264
John Hani Mounir <30793239+JohnMounir@users.noreply.github.com>
2019-04-15 01:44:28 +0200

    Add files via upload

---
+++ Code/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.example.ssm;
+
+
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
